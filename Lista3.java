7e5543ccbc80dfabc7bc51b8a53c1eb59287e4f3
Ana Carolina <anacarolynaac25@gmail.com>
2017-03-19 23:14:21 -0300

    exercicio

---
+++ Lista3.java
@@ -0,0 +1,9 @@
+package exercícios;
+
+public class Lista3 {
+    public static void main(String[] args) {
+        double res;
+        res = (5*6+2)/3;
+        System.out.println("O resultado é "+res);
+    }
+}
